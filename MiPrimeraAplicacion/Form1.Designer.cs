507e84ed011580e89bb65fe25b26c75d4334f392
Diego Gonzalez <diegonzalezv93@gmail.com>
2023-07-30 00:02:20 -0300

    controles, linklabel control label

--- MiPrimeraAplicacion/Form1.Designer.cs
+++ MiPrimeraAplicacion/Form1.Designer.cs
@@ -28,7 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulario));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Yellow;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Image = ((System.Drawing.Image)(resources.GetObject("btn1.Image")));
+            this.btn1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn1.Location = new System.Drawing.Point(561, 163);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(101, 36);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "botoncito";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl1.Font = new System.Drawing.Font("JetBrains Mono NL", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbl1.Location = new System.Drawing.Point(315, 163);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(122, 23);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Botoncito: ";
+            this.lbl1.MouseLeave += new System.EventHandler(this.lbl1_MouseLeave);
+            this.lbl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lbl1_MouseMove);
             // 
             // Formulario
             // 
@@ -36,19 +69,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Formulario";
-            this.Opacity = 0.75D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Formulario";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.Formulario_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl1;
     }
 }
 
--- MiPrimeraAplicacion/Form1.cs
+++ MiPrimeraAplicacion/Form1.cs
@@ -26,5 +26,31 @@ namespace MiPrimeraAplicacion
         {
             MessageBox.Show("Chau chau...");
         }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Se disparo el evento Click", "Atención");
+            //this.BackColor = Color.LightGoldenrodYellow;
+            MouseEventArgs click = (MouseEventArgs)e;
+            if (click.Button == MouseButtons.Left)
+                MessageBox.Show("Presiono el botón Izquierdo", "Atención");
+            else if (click.Button == MouseButtons.Right)
+                MessageBox.Show("Presiono el Botón Derecho", "Atención");
+            else
+            if (click.Button == MouseButtons.Middle)
+                MessageBox.Show("Presiono el botón del Medio", "Atención");
+        }
+
+        private void lbl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            lbl1.BackColor= Color.Cyan;
+            lbl1.Cursor = Cursors.Hand;
+        }
+
+        private void lbl1_MouseLeave(object sender, EventArgs e)
+        {
+            lbl1.BackColor = System.Drawing.SystemColors.Control;
+            lbl1.Cursor = Cursors.Arrow;
+        }
     }
 }
